4ff002263e4a8078eb55468a42c09a2f4599b297
kirin <wangchiming1977@gmail.com>
2020-05-20 10:38:24 +0800

    20200520

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+// middleware寫在頂層
+// 錄影檔看20200519下午15:30
+// 只能用的格式=>application/x-www-form-urlencoded和application/json(傳送方法)
+// fetch用的是這種方法
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get('/', (req, res)=>{
-    res.render('main', {name: 'Vic'});
+    res.render('main', {name: 'Kirin',pageTitle:'kirin的網站'});
 });
 
 // 只有路由要給斜線
@@ -17,6 +24,51 @@ app.get('/sales-json', (req, res)=>{
     // 下面的sales-json是template不需要加斜線
     res.render('sales-json', { sales })
 });
+
+app.get('/try-qs', (req, res)=>{
+    res.json(req.query);
+})
+
+
+// app.get('/try-post-form', (req, res)=>{
+//     res.render('try-post-form',req.body);    // 有給資料
+// })
+// 取得urlencoded parser, 不使用qs lib, 而使用內建的querystring lib
+// true就是使用qs套件的parse，false就是使用本身的bodyParser
+// const urlencodedParser = express.urlencoded({extended: false});
+// middleware
+// urlencodedParser這個仲介軟體會塞到req.body，然後下面再把它轉成json回傳回去。
+// app.post('/try-post-form', urlencodedParser, (req, res)=>{
+//     req.body.haha = 'shin';
+//     res.json(req.body);
+// })
+
+// 如果middleware寫在頂層，則用下方的寫法
+// app.get('/try-post-form', (req, res)=>{
+//     res.render('try-post-form');    // 沒給資料
+// })
+// app.post('/try-post-form', (req, res)=>{
+//     req.body.haha = 'shin';
+//     res.json(req.body);
+// })
+
+// 表單post
+app.get('/try-post-form', (req, res)=>{
+    res.render('try-post-form', {pageTitle:'測試表單'})
+})
+app.post('/try-post-form', (req, res)=>{
+    // req.body.haha = 'shin';
+    req.body.pageTitle = '測試表單-Post'
+    res.render('try-post-form', req.body)
+})
+
+// 測試postman json post
+app.post('/try-json-post', (req, res)=>{
+    req.body.haha = 'shin';
+    req.body.pageTitle = '測試表單-Json'
+    req.body.contentType = req.get('Content-Type'); // 取得檔頭
+    res.json(req.body)
+})
 
 app.get('/pending', (req, res)=>{
 
